a2a200151f95ac1cb7dd84a82939715c746b2d72
KARTIKrocks <kartik.rajput622001@gmail.com>
2023-04-10 21:35:24 +0530

    added bubble sort

--- sorting_algorithms/01_bubble_sort/main.go
+++ sorting_algorithms/01_bubble_sort/main.go
@@ -1,6 +1,7 @@
 // Golang program for implementation of Bubble Sort
 
-/* Given an unordered list, we compare adjacent elements in the list,
+/* 
+Given an unordered list, we compare adjacent elements in the list,
 each time, putting in the right order of magnitude, only two elements. The algorithm hinges on a swap procedure. 
 Knowing how many times to swap is important when implementing a bubble sort algorithm. To sort a list of numbers such as [3, 2, 1], we need to swap the elements a maximum of twice. 
 This is equal to the length of the list minus 1
