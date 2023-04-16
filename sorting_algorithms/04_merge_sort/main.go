69713583d62d2345f54236a6a2aeca89423aa74e
KARTIKrocks <kartik.rajput622001@gmail.com>
2023-04-16 21:21:23 +0530

    added file heading in mergeSort/main.go file

---
+++ sorting_algorithms/04_merge_sort/main.go
@@ -0,0 +1,61 @@
+// Golang program for implementation of Merge Sort
+
+/* 
+Merge Sort is a Divide and Conquer algorithm. Meaning, the algorithm splits an input into various pieces, 
+sorts them and then merges them back together. It divides input slice in two halves, calls itself for the two halves and then merges the two sorted halves. 
+The merge() function is used for merging two halves.
+*/
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func mergeSort(array []int) []int {
+	n := len(array)
+	if n <= 1 {
+		return array
+	}
+	mid := n / 2
+	left := mergeSort(array[:mid])
+	right := mergeSort(array[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0)
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
+
+// Generates a slice of size, size filled with random numbers
+func generateSlice(size int) []int {
+
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
+
+func main() {
+	slice := generateSlice(10)
+	fmt.Println("--- Unsorted --- \n", slice)
+	slice = mergeSort(slice)
+	fmt.Println("--- Sorted ---\n", slice)
+}
